32c82d764bb4254a2a350e6a3b9b4e3dbd6db492
Luan <luanoliveira17@hotmail.com>
2022-10-17 13:46:03 -0300

    Using in memory repository

--- src/useCases/createAppointment.spec.ts
+++ src/useCases/createAppointment.spec.ts
@@ -2,10 +2,12 @@ import { describe, expect, it } from 'vitest'
 import { Appointment } from '../entities/appointment'
 import { CreateAppointment } from './createAppointment'
 import { getFutureDate } from '../tests/utils/getFutureDate'
+import { InMemoryAppointmentsRepository } from '../repositories/inMemory/inMemoryAppointmentsRepository'
 
 describe('Create Appointment', () => {
   it('should be able to create an appointment', () => {
-    const createAppointment = new CreateAppointment()
+    const appointmentsRepository = new InMemoryAppointmentsRepository()
+    const createAppointment = new CreateAppointment(appointmentsRepository)
 
     const startsAt = getFutureDate('2022-10-17')
     const endsAt = getFutureDate('2022-10-18')
@@ -17,5 +19,51 @@ describe('Create Appointment', () => {
         endsAt
       })
     ).resolves.toBeInstanceOf(Appointment)
+  })
+
+  it('should not be able to create an appointment with overlapping dates', async () => {
+    const appointmentsRepository = new InMemoryAppointmentsRepository()
+    const createAppointment = new CreateAppointment(appointmentsRepository)
+
+    const startsAt = getFutureDate('2022-10-17')
+    const endsAt = getFutureDate('2022-10-20')
+
+    await createAppointment.execute({
+      customer: 'John Doe',
+      startsAt,
+      endsAt
+    })
+
+    expect(
+      createAppointment.execute({
+        customer: 'John Doe',
+        startsAt: getFutureDate('2022-10-18'),
+        endsAt: getFutureDate('2022-10-21')
+      })
+    ).rejects.toBeInstanceOf(Error)
+
+    expect(
+      createAppointment.execute({
+        customer: 'John Doe',
+        startsAt: getFutureDate('2022-10-15'),
+        endsAt: getFutureDate('2022-10-19')
+      })
+    ).rejects.toBeInstanceOf(Error)
+
+    expect(
+      createAppointment.execute({
+        customer: 'John Doe',
+        startsAt: getFutureDate('2022-10-15'),
+        endsAt: getFutureDate('2022-10-21')
+      })
+    ).rejects.toBeInstanceOf(Error)
+
+    expect(
+      createAppointment.execute({
+        customer: 'John Doe',
+        startsAt: getFutureDate('2022-10-18'),
+        endsAt: getFutureDate('2022-10-19')
+      })
+    ).rejects.toBeInstanceOf(Error)
   })
 })
